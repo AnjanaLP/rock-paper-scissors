ed19e8eae1b28ae9978ad1d7ff6b495a9e084f06
Anjana <anjana.lpn@gmail.com>
2019-05-28 16:20:41 +0100

    User Story 3 As a player So that I can play the game I want to choose a weapon

--- app.rb
+++ app.rb
@@ -14,7 +14,13 @@ class RPS < Sinatra::Base
 
   get '/play' do
     @player_name = session[:player_name]
+    @player_weapon = session[:player_weapon]
     erb :play
+  end
+
+  post '/play' do
+    session[:player_weapon] = params[:player_weapon]
+    redirect '/play'
   end
 
   run! if app_file == $0
--- spec/features/play_spec.rb
+++ spec/features/play_spec.rb
@@ -2,10 +2,19 @@ feature 'Play the game' do
   # As a player
   # So that I can play the game
   # I want to be presented with the weapon choices
-  scenario 'see the weapon choices' do
+  scenario 'player sees the weapon choices' do
     register_name
     expect(page).to have_button "rock"
     expect(page).to have_button "paper"
     expect(page).to have_button "scissors"
+  end
+
+  # As a player
+  # So that I can play the game
+  # I want to choose a weapon
+  scenario 'player chooses a weapon' do
+    register_name
+    click_button "rock"
+    expect(page).to have_content "You chose rock!"
   end
 end
