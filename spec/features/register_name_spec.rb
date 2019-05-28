5746205ca0b0fb09d0167bc59634f97dcfa043f9
Anjana <anjana.lpn@gmail.com>
2019-05-28 15:35:25 +0100

    User Story 1 As a player So that I can see my name I want to register my name before playing

---
+++ spec/features/register_name_spec.rb
@@ -0,0 +1,11 @@
+# As a player
+# So that I can see my name
+# I want to register my name before playing
+feature 'Register name' do
+  scenario 'register name and see it' do
+    visit '/'
+    fill_in "Name", with: "Anjana"
+    click_button "Submit"
+    expect(page).to have_content "Hello, Anjana!"
+  end
+end
